c68cf91886cbeaf4da26087bbdbe56cb8b30f692
chrismac <zerves@gmail.com>
2019-05-06 13:27:48 -0700

    direction

--- physicalobject.py
+++ physicalobject.py
@@ -22,10 +22,24 @@ class PhysicalObject(pyglet.sprite.Sprite):
         self.y += self.velocity_y * dt
 
     def check_collision(self, other):
-        if (self.x < other.x + other.width
+        """ if (self.x < other.x + other.width
             and self.x + other.width > other.x
             and self.y < other.y + other.height
             and self.y + self.height > other.y):
             #print("collision")
             return True
-        return False
+        return False """
+
+        b_collision = other.get_bottom() - self.y
+        t_collision = self.get_bottom() - other.y
+        l_collision = self.get_right() - other.x
+        r_collision = other.get_right() - self.x
+
+        if t_collision < b_collision and t_collision < l_collision and t_collision < r_collision:
+            #top collision
+        if b_collision < t_collision and b_collision < l_collision and b_collision < r_collision:
+            #bottom collision
+        if l_collision < r_collision and l_collision < t_collision and l_collision < b_collision:
+            #left collision
+        if r_collision < l_collision and r_collision < t_collision and r_collision < b_collision:
+            #right collision
